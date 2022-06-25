60d2ce43ee3e9a51f39de36e66ce7a8c042b712c
Alexey <77731459+goooodnes@users.noreply.github.com>
2022-06-26 06:25:17 +0800

    Add files via upload

---
+++ icad.js
@@ -0,0 +1,54 @@
+window.onload = async () => {
+    if (!window.keplr) {
+        alert("Please install keplr extension");
+    } else 
+    await window.keplr.experimentalSuggestChain({
+    chainId: "kqcosmos-1",
+    chainName: "ICAD",
+    rpc: "http://162.55.235.69:2865", // replace with your rpc url
+    rest: "http://162.55.235.69:1387", // replace with your rest api url
+    bip44: {
+        coinType: 118,
+    },
+    bech32Config: {
+        bech32PrefixAccAddr: "cosmos",
+        bech32PrefixAccPub: "cosmos" + "pub",
+        bech32PrefixValAddr: "cosmos" + "valoper",
+        bech32PrefixValPub: "cosmos" + "valoperpub",
+        bech32PrefixConsAddr: "cosmos" + "valcons",
+        bech32PrefixConsPub: "cosmos" + "valconspub",
+    },
+    currencies: [ 
+        { 
+            coinDenom: "ATOM", 
+            coinMinimalDenom: "uatom", 
+            coinDecimals: 6, 
+            coinGeckoId: "uatom", 
+        }, 
+    ],
+    feeCurrencies: [
+        {
+            coinDenom: "ATOM",
+            coinMinimalDenom: "uatom",
+            coinDecimals: 6,
+            coinGeckoId: "uatom",
+        },
+    ],
+    stakeCurrency: {
+        coinDenom: "ATOM",
+        coinMinimalDenom: "uatom",
+        coinDecimals: 6,
+        coinGeckoId: "uatom",
+    },
+    coinType: 118,
+    gasPriceStep: {
+        low: 0.01,
+        average: 0.025,
+        high: 0.03,
+    },
+    
+    features: [
+        "cosmwasm", "ibc-transfer", "ibc-go", "wasmd_0.24+"
+        ],
+});
+}
